49a91516679fec814e0d82d4d56f6018b285112f
Maxim Belov <mvbrocs@gmail.com>
2017-01-22 22:37:20 +0300

    Build project v2

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,8 @@ gulp.task("serve", function() {
 	});
 
 	gulp.watch("less/**/*.less", ["style"]);
-	gulp.watch("*.html").on("change", server.reload);
+	gulp.watch("*.html")
+    .on("change", server.reload);
 });
 
 gulp.task("images", function() {
