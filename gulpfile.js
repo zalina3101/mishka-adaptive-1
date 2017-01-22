2f8960b09fcb2293baf0872acc8cee94ac62cd8a
Maxim Belov <mvbrocs@gmail.com>
2017-01-22 10:54:48 +0300

    Add plugins for auto v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,34 @@
-var gulp = require("gulp");
-var less = require("gulp-less");
+"use strict";
+
+var gulp         = require("gulp");
+var less         = require("gulp-less");
+var plumber      = require("gulp-plumber");
+var postcss      = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server 			 = require("browser-sync");
 
 gulp.task("style", function() {
 	gulp.src("less/style.less")
+		.pipe(plumber())
 		.pipe(less())
-		.pipe(gulp.dest("css"));
+		.pipe(postcss([
+			autoprefixer({browsers: [
+				"last 1 version",
+				"last 2 Chrome versions",
+				"last 2 Firefox versions",
+				"last 2 Opera versions",
+				"last 2 Edge versions"
+			]})
+		]))
+		.pipe(gulp.dest("css"))
+		.pipe(server.reload({stream: true}));
+});
+
+gulp.task("serve", ["style"], function() {
+	server.init({
+		server: "."
+	});
+
+	gulp.watch("less/**/*.less", ["style"]);
+	gulp.watch("*.html").on("change", server.reload);
 });
