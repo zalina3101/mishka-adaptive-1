865c231a685dc2c7dd7acc2bbbfaff49b8ded7ba
Maxim Belov <mvbrocs@gmail.com>
2017-01-22 14:19:49 +0300

    Added plugins for optim

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ var plumber      = require("gulp-plumber");
 var postcss      = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server 			 = require("browser-sync");
+var mqpacker     = require("css-mqpacker");
+var minify       = require("gulp-csso");
+var rename			 = require("gulp-rename");
+var imagemin		 = require("gulp-imagemin");
+var svgmin       = require("gulp-svgmin");
+var svgstore     = require("gulp-svgstore");
 
 gulp.task("style", function() {
 	gulp.src("less/style.less")
@@ -18,8 +24,14 @@ gulp.task("style", function() {
 				"last 2 Firefox versions",
 				"last 2 Opera versions",
 				"last 2 Edge versions"
-			]})
+			]}),
+			mqpacker({
+				sort: true
+			})
 		]))
+		.pipe(gulp.dest("css"))
+		.pipe(minify())
+		.pipe(rename("style.min.css"))
 		.pipe(gulp.dest("css"))
 		.pipe(server.reload({stream: true}));
 });
@@ -32,3 +44,22 @@ gulp.task("serve", ["style"], function() {
 	gulp.watch("less/**/*.less", ["style"]);
 	gulp.watch("*.html").on("change", server.reload);
 });
+
+gulp.task("images", function() {
+	return gulp.src("img/**/*.{png,jpg,gif}")
+		.pipe(imagemin([
+			imagemin.optipng({optimizationLevel: 3}),
+			imagemin.jpegtran({progressive: true})
+		]))
+		.pipe(gulp.dest("img"));
+});
+
+gulp.task("symbols", function() {
+	return gulp.src("img/icons/*.svg")
+		.pipe(svgmin())
+		.pipe(svgstore({
+			inlineSvg: true
+		}))
+		.pipe(rename("symbols.svg"))
+		.pipe(gulp.dest("img"));
+})
