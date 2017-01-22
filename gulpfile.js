086682bcbd1d7f1bd405e2fd7d5614e0cb8630f8
Maxim Belov <mvbrocs@gmail.com>
2017-01-22 20:59:49 +0300

    Comlete tasks lec8

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var rename			 = require("gulp-rename");
 var imagemin		 = require("gulp-imagemin");
 var svgmin       = require("gulp-svgmin");
 var svgstore     = require("gulp-svgstore");
+var run          = require("run-sequence");
+var del          = require("del");
 
 gulp.task("style", function() {
 	gulp.src("less/style.less")
@@ -29,16 +31,16 @@ gulp.task("style", function() {
 				sort: true
 			})
 		]))
-		.pipe(gulp.dest("css"))
+		.pipe(gulp.dest("build/css"))
 		.pipe(minify())
 		.pipe(rename("style.min.css"))
-		.pipe(gulp.dest("css"))
+		.pipe(gulp.dest("build/css"))
 		.pipe(server.reload({stream: true}));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("serve", function() {
 	server.init({
-		server: "."
+		server: "build"
 	});
 
 	gulp.watch("less/**/*.less", ["style"]);
@@ -46,20 +48,48 @@ gulp.task("serve", ["style"], function() {
 });
 
 gulp.task("images", function() {
-	return gulp.src("img/**/*.{png,jpg,gif}")
+	return gulp.src("build/img/**/*.{png,jpg,gif}")
 		.pipe(imagemin([
 			imagemin.optipng({optimizationLevel: 3}),
 			imagemin.jpegtran({progressive: true})
 		]))
-		.pipe(gulp.dest("img"));
+		.pipe(gulp.dest("build/img"));
 });
 
 gulp.task("symbols", function() {
-	return gulp.src("img/icons/*.svg")
+	return gulp.src("build/img/icons/*.svg")
 		.pipe(svgmin())
 		.pipe(svgstore({
 			inlineSvg: true
 		}))
 		.pipe(rename("symbols.svg"))
-		.pipe(gulp.dest("img"));
-})
+		.pipe(gulp.dest("build/img"));
+});
+
+gulp.task("build", function(fn) {
+  run(
+    "clean",
+    "copy",
+    "style",
+    "images",
+    "symbols",
+    fn
+  );
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
