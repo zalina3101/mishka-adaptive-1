3ff7a594c3f18895610a0f0f742f72396b035ede
Maxim Belov <mvbrocs@gmail.com>
2017-01-29 22:35:51 +0300

    Start dev sec main-header

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 
-var navMenu = document.querySelector(".main-nav");
+var navMenu   = document.querySelector(".main-nav__items");
 
 $(".js-nav-toggle").on("click", function() {
-  navMenu.classList.toggle("main-nav--closed");
+  $(this).toggleClass("main-nav__toggle--opened");
+  if (!navMenu.classList.contains("main-nav--opened")) {
+    navMenu.classList.add("main-nav--opened");
+    if (navMenu.classList.contains("main-nav--closed")) {
+      navMenu.classList.remove("main-nav--closed");
+    }
+  } else {
+    navMenu.classList.remove("main-nav--opened");
+    if (!navMenu.classList.contains("main-nav--closed")) {
+      navMenu.classList.add("main-nav--closed");
+    } else {
+      navMenu.classList.remove("main-nav--closed");
+    }
+  }
 });
