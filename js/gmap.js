525d504096e5363cdc9653a308ccc8c5f2202609
Maxim Belov <mvbrocs@gmail.com>
2017-02-05 00:10:30 +0300

    Comlete html css index-mobile

---
+++ js/gmap.js
@@ -0,0 +1,24 @@
+var latlng = new google.maps.LatLng("59.938783", "30.323062");
+
+var myOptions = {
+  zoom: 17,
+  center: latlng,
+  navigationControlOptions: {
+    style: google.maps.NavigationControlStyle.SMALL
+  },
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("google-map"),myOptions);
+map.scrollwheel=true;
+map.setOptions({ mapTypeControl:true});
+
+var marker = new google.maps.Marker({
+  position: {lat: 59.938783, lng: 30.323062},
+  map: map,
+  title: 'Mishka',
+  icon: {
+    url: "../img/icon-map.png",
+    scaledSize: new google.maps.Size(67, 100)
+  }
+});
