a648688e7be3120aac2ee10b55c7ad502eb8870f
mbelov <mvbrocs@gmail.com>
2017-02-06 17:34:01 +0300

    Working with header index-tablet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 
 var navMenu   = document.querySelector(".main-nav__items");
-// var mqT = window.matchMedia( "(min-width: 768px)" ); // mq for tablet width
-// var logoImg = document.querySelector(".page-header__logo img");
-
+var mainNavOpened = 0;
+var mainNavClosed = 0;
 // Main nav Toggle
 
 $(".js-nav-toggle").on("click", function() {
@@ -22,7 +21,39 @@ $(".js-nav-toggle").on("click", function() {
   }
 });
 
-//Change Logo at tablet width
+// media query event handler
+if (matchMedia) {
+  var mq = window.matchMedia("(min-width: 768px)");
+  mq.addListener(WidthChange);
+  WidthChange(mq);
+}
+
+// media query change
+function WidthChange(mq) {
+  if (navMenu.classList.contains("main-nav--closed")) {
+    mainNavClosed = 1;
+  } else if (navMenu.classList.contains("main-nav--opened")) {
+    mainNavOpened = 1;
+  }
+  if (mq.matches) {
+     console.log("IN1", mainNavOpened, mainNavClosed);
+    if (mainNavOpened) {
+      navMenu.classList.remove("main-nav--opened");
+    } else if (mainNavClosed) {
+      navMenu.classList.remove("main-nav--closed");
+    }
+
+  } else {
+    console.log("IN2", mainNavOpened, mainNavClosed);
+    if (mainNavOpened) {
+      navMenu.classList.add("main-nav--opened");
+    } else if (mainNavClosed) {
+      navMenu.classList.add("main-nav--closed");
+    }
+
+  }
+
+}
 
 // if (mqT.matches) {
 //   logoImg.src = "../img/logo-tablet.svg"
