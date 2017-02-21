189608b41d9981ad7351e2a07ccf10370f81b025
mbelov <mvbrocs@gmail.com>
2017-02-21 17:54:44 +0300

    rewrite main-nav

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var mainNav = document.querySelector(".main-nav");
+var navToggle = document.querySelector(".js-nav-toggle");
+
+mainNav.classList.remove("main-nav--no-js");
+
+navToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("main-nav--closed");
+  mainNav.classList.toggle("main-nav--opened");
+});
+
+
