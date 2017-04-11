6455a899e1f8cf1b603795836e14a0a1e0ea01e6
Maxim Belov <mvbrocs@gmail.com>
2017-04-11 21:16:52 +0300

    make slider

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-var mainNav = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".js-nav-toggle");
+var mainNav   = document.querySelector(".main-nav"),
+    navToggle = document.querySelector(".js-nav-toggle"),
+    sliderList = document.querySelector(".reviews__list"),
+    btnPrev   = document.querySelector(".reviews__slider-btn--prev"),
+    btnNext   = document.querySelector(".reviews__slider-btn--next");
+    shiftSlide = 0;
 
 mainNav.classList.remove("main-nav--no-js");
 
@@ -7,6 +11,35 @@ navToggle.addEventListener("click", function() {
   mainNav.classList.toggle("main-nav--closed");
   mainNav.classList.toggle("main-nav--opened");
 });
+
+btnPrev.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (shiftSlide === 0) {
+    shiftSlide += -69;
+  } else if (shiftSlide === -69) {
+    shiftSlide += 34.5;
+  } else {
+    shiftSlide = 0;
+  }
+
+  sliderList.style.transform = "translateX(" + shiftSlide + "%)";
+});
+
+btnNext.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  if (shiftSlide === 0) {
+    shiftSlide += -34.5;
+  } else if (shiftSlide === -34.5) {
+    shiftSlide += -34.5;
+  } else {
+    shiftSlide = 0;
+  }
+
+  sliderList.style.transform = "translateX(" + shiftSlide + "%)";
+});
+
 
 
 
