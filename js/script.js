8a5460d965e88285cfbd7823766ac35bfd8b73bb
Maxim Belov <mvbrocs@gmail.com>
2017-04-12 11:52:42 +0300

    make disable class for btn reviews

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-var mainNav   = document.querySelector(".main-nav"),
-    navToggle = document.querySelector(".js-nav-toggle"),
+"use strict";
+
+var mainNav    = document.querySelector(".main-nav"),
+    navToggle  = document.querySelector(".js-nav-toggle"),
     sliderList = document.querySelector(".reviews__list"),
-    btnPrev   = document.querySelector(".reviews__slider-btn--prev"),
-    btnNext   = document.querySelector(".reviews__slider-btn--next");
+    btnPrev    = document.querySelector(".reviews__slider-btn--prev"),
+    btnNext    = document.querySelector(".reviews__slider-btn--next"),
     shiftSlide = 0;
 
 mainNav.classList.remove("main-nav--no-js");
@@ -15,26 +17,32 @@ navToggle.addEventListener("click", function() {
 btnPrev.addEventListener("click", function(event) {
   event.preventDefault();
 
-  if (shiftSlide === 0) {
-    shiftSlide += -69;
-  } else if (shiftSlide === -69) {
-    shiftSlide += 34.5;
-  } else {
+  if (btnNext.classList.contains("reviews__slider-btn--disabled")) {
+    btnNext.classList.remove("reviews__slider-btn--disabled");
+  }
+
+  if (shiftSlide === -34.5) {
     shiftSlide = 0;
+  } else if (shiftSlide === -69) {
+    shiftSlide = -34.5;
   }
 
   sliderList.style.transform = "translateX(" + shiftSlide + "%)";
+
+  if (shiftSlide === 0) {
+    btnPrev.classList.add("reviews__slider-btn--disabled");
+  }
 });
 
 btnNext.addEventListener("click", function(event) {
   event.preventDefault();
 
+  btnPrev.classList.remove("reviews__slider-btn--disabled");
+
   if (shiftSlide === 0) {
-    shiftSlide += -34.5;
+    shiftSlide = -34.5;
   } else if (shiftSlide === -34.5) {
-    shiftSlide += -34.5;
-  } else {
-    shiftSlide = 0;
+    shiftSlide = -69;
   }
 
   sliderList.style.transform = "translateX(" + shiftSlide + "%)";
